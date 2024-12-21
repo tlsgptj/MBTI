9b10a11730f8782ad9760efdae56fe69f65b3326
신혜서 <125953570+tlsgptj@users.noreply.github.com>
2024-12-21 18:56:04 +0900

    배열을 구성하여 넣어줌

--- mbti_color/src/app/components/main.tsx
+++ mbti_color/src/app/components/main.tsx
@@ -1,5 +1,24 @@
+import mock from 'mbti_color\src\data\mock.json';
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
+
 function main() {
-    return <div>Home</div>
+    const [filter, setFilter] = useState(null);
+    return (
+        <div>
+            <h1>MBTI별 좋아하는 컬러</h1>
+            <div>
+                <Link to="/new">+ 새 컬러 등록하기</Link>
+                <ul>
+                    {mock.map((item) => (
+                        <li key={item.id}>
+                            {item.id} {item.mbti} --- {item.colorCode}
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </div>
+    );
 }
 
 export default Home;
